1ebe1bf1777d5048398484d4fa699d6d80524bb8
Jhost601 <hostetjd@dukes.jmu.edu>
2026-09-16 10:05:50 -0400

    Hw

--- Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.2 - Odds and Evens Count/odd_evens_count_template.py
+++ Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.2 - Odds and Evens Count/odd_evens_count_template.py
@@ -28,4 +28,13 @@ nums = generate_random_int_list(max_length, upper_bound)
 num_evens = 0
 num_odds = 0
 
+print(nums)
 ### YOUR CODE BEGINS HERE ###
+n = len(nums)
+for n in nums:
+    if n % 2 == 0:
+        num_evens += 1
+    else:
+        num_odds += 1
+print(num_evens)
+print(num_odds)
